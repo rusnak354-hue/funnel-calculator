d10c9f47f44d7200c4a0f2d37c44f9b57eae4f6e
rusnak354-hue <rusnak354@gmail.com>
2026-03-17 17:50:40 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // ВАЖЛИВО: Цей рядок каже шукати стилі в усіх файлах папки src
   ],
+  // ... інші налаштування
+}
   theme: {
     extend: {},
   },
