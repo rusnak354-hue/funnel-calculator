1513553377beee93ca7b127bdf1759e2eb5362ed
rusnak354-hue <rusnak354@gmail.com>
2026-03-17 18:00:58 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import FunnelCalculator from './FunnelCalculator'
 
-// Якщо у вас є файл із загальними стилями (наприклад, index.css), 
-// розкоментуйте наступний рядок:
-// import './index.css' 
+// Підключаємо стилі з файлу index.css
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
