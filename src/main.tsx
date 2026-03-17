d7a15ee28b3f3e558e57e8a6d60fc4d0345a151d
rusnak354-hue <rusnak354@gmail.com>
2026-03-17 16:19:32 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import FunnelCalculator from './Index'
+import FunnelCalculator from './FunnelCalculator'  // ← змінено
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
